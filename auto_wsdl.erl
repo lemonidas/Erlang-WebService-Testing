eae6ba88ca7ed53022f4c7d606f950607909273e
Leonidas Lampropoulos <lemonidas_13@hotmail.com>
2011-10-04 15:35:12 +0300

    Added Comments and fixed minor bug in auto_wsdl

--- auto_wsdl.erl
+++ auto_wsdl.erl
@@ -14,6 +14,7 @@ parse_wsdl(WSDL_url, Prefix, Module_name) ->
   Wsdl = yaws_soap_lib:initModel(WSDL_url, Prefix),
   Operations = yaws_soap_lib:wsdl_operations(Wsdl),
   Model = yaws_soap_lib:wsdl_model(Wsdl),
+  % FIXME: Now only first operation is handled
   [H | _Tail] = Operations,
   Op = yaws_soap_lib:wsdl_op_operation(H),
   Call_text = make_call_text(WSDL_url, Prefix, Op),
@@ -30,6 +31,9 @@ create_property(Model, Operation, Prefix) ->
   Request_types = find_operation(Types, Operation_atom),
   Generators = create_generators(Request_types),
   {A1,A2} = create_args (Generators),
+% Created Property is name prop_<op_name>()
+% Variables are named X1, X2 (in reverse order)
+% To make call we need to replace "tuple" structure of A1 to list
 " 
 prop_"++Operation ++"() ->
   ?FORALL(" ++ A1 ++ ", "++ A2 ++ ", 
@@ -42,12 +46,14 @@ create_args([], A1 , A2, _) ->
 create_args([{H}|T], A1, A2, N) ->
   create_args(T, (", X"++(integer_to_list(N))++A1), (", "++ H ++ A2), N+1).
 
+% Creates the header of the produced file
 create_header(Module_name) ->
 "-module(" ++ Module_name ++ ").
 
 -include_lib(\"proper/include/proper.hrl\").
 ".
 
+% Creates the auto_make_call function that handles soapy things...
 make_call_text(WSDL_url, Prefix, Op_name)->
 " 
 not_empty(T) -> ?SUCHTHAT(L,T,L=/=[]).
@@ -62,6 +68,8 @@ auto_make_call(Args) ->
   end.
 ".
 
+
+% Go throught the Types list to find the operation Op_atom and return the Types it expects
 find_operation([H | T], Op_atom) ->
   case H of
     {type, Op_atom, _type, Data, _, _, _, _, _, _, _ ,_} ->
@@ -72,6 +80,8 @@ find_operation([H | T], Op_atom) ->
 
 create_generators(Request_types) -> create_generators(Request_types, []).
 
+% Creates the PropEr generators for the specified Type
+% Currently handles single nested complex types/sequences of ints/floats/bools
 create_generators([], Acc) -> Acc;
 create_generators([{el, [Type], Min_occurs, Max_occurs, _no}|T], Acc) ->
   Gen_type = extract_gen_type (Type),
@@ -82,6 +92,7 @@ create_generators([{el, [Type], Min_occurs, Max_occurs, _no}|T], Acc) ->
     _ -> not_handled_yet
   end.
 
+% Extract the PropEr generator
 extract_gen_type ({_alt, _name, {'#PCDATA', GenType}, _, _, _, _,_})-> 
   case GenType of
     integer -> "integer()";
